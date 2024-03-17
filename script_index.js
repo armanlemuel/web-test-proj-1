072b835248b09aa28a6959a6afa8a1eb60282668
armanlemuel <39437752+armanlemuel@users.noreply.github.com>
2024-03-17 23:02:11 +0800

    Add files via upload

---
+++ script_index.js
@@ -0,0 +1,16 @@
+// Firebase initialization code
+const firebaseConfig = {
+    apiKey: "AIzaSyAzATpDuHaHbhMlwtz-OJd7FnFATvTqVk4",
+    authDomain: "web-test-proj-1.firebaseapp.com",
+    databaseURL: "https://web-test-proj-1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "web-test-proj-1",
+    storageBucket: "web-test-proj-1.appspot.com",
+    messagingSenderId: "284556723754",
+    appId: "1:284556723754:web:c463a13a6d85defd9e6553",
+    measurementId: "G-0GEK8VNVNM"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  // Other JavaScript code for your application
+  // This is where you handle DOM manipulation, user interactions, and Firebase operations  
